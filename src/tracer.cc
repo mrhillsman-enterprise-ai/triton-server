b8078a8ff94324edcfbf56ff8be7fcb6e0d9b59d
Iman Tabrizian <itabrizian@nvidia.com>
2023-02-27 13:06:55 -0500

    Add tag to the server trace

--- src/tracer.cc
+++ src/tracer.cc
@@ -769,7 +769,7 @@ void
 TraceManager::TraceActivity(
     TRITONSERVER_InferenceTrace* trace,
     TRITONSERVER_InferenceTraceActivity activity, uint64_t timestamp_ns,
-    void* userp)
+    void* userp, const char* tag)
 {
   uint64_t id;
   LOG_TRITONSERVER_ERROR(
@@ -842,9 +842,15 @@ TraceManager::TraceActivity(
     *ss << "},";
   }
 
-  *ss << "{\"id\":" << id << ",\"timestamps\":["
-      << "{\"name\":\"" << TRITONSERVER_InferenceTraceActivityString(activity)
-      << "\",\"ns\":" << timestamp_ns << "}]}";
+  *ss << "{\"id\":" << id << ",\"timestamps\":[";
+
+  if (activity != TRITONSERVER_TRACE_CUSTOM_ACTIVITY) {
+    *ss << "{\"name\":\""
+        << TRITONSERVER_InferenceTraceActivityString(activity);
+  } else {
+    *ss << "{\"name\":\"" << tag;
+  }
+  *ss << "\",\"ns\":" << timestamp_ns << "}]}";
 }
 
 void
